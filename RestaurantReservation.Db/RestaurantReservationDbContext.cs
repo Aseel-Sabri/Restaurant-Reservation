c0191bae430f25f43c50654dcd94f5aeaca3a4aa
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-10-07 17:41:30 +0300

    Create RestaurantReservationDbContext

---
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Db;
+
+public class RestaurantReservationDbContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<MenuItem> MenuItems { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Table> Tables { get; set; }
+
+    public RestaurantReservationDbContext(DbContextOptions<RestaurantReservationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<MenuItem>()
+            .HasKey(item => item.ItemId);
+    }
+}
---
+++ RestaurantReservation.Db/RestaurantReservationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace RestaurantReservation.Db;
+
+public class RestaurantReservationDbContextFactory : IDesignTimeDbContextFactory<RestaurantReservationDbContext>
+{
+    public RestaurantReservationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<RestaurantReservationDbContext>();
+        // TODO
+        optionsBuilder.UseSqlServer(
+            "Server=localhost;Database=RestaurantReservationCore;Integrated Security=True;Encrypt=False"
+        );
+        return new RestaurantReservationDbContext(optionsBuilder.Options);
+    }
+}
