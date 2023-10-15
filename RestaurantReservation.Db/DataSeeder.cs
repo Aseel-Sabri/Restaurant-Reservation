b2b7bb179bd7ca4ab33d53145a3d2e0ed52cc7e7
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-10-15 14:08:22 +0300

    Update DataSeeder: Convert to static data

--- RestaurantReservation.Db/DataSeeder.cs
+++ RestaurantReservation.Db/DataSeeder.cs
@@ -12,8 +12,9 @@ public static class DataSeeder
         SeedTablesTable(modelBuilder);
         SeedEmployeesTable(modelBuilder);
         SeedReservationsTable(modelBuilder);
-        var menuItems = SeedMenuItemsTable(modelBuilder);
-        SeedOrdersAndOrderItemsTables(modelBuilder, menuItems);
+        SeedMenuItemsTable(modelBuilder);
+        SeedOrdersTable(modelBuilder);
+        SeedOrderItemsTable(modelBuilder);
     }
 
     private static void SeedCustomersTable(ModelBuilder modelBuilder)
@@ -114,21 +115,129 @@ public static class DataSeeder
 
     private static void SeedTablesTable(ModelBuilder modelBuilder)
     {
-        var tables = new List<Table>();
-
-        int numOfTables = 20, reservationsPerRestaurant = 4;
-        var random = new Random();
-        int minCapacity = 2, maxCapacity = 10;
-
-        for (int i = 0; i < numOfTables; i++)
+        var tables = new List<Table>()
         {
-            tables.Add(new Table
+            new Table()
             {
-                TableId = i + 1,
-                RestaurantId = (i / reservationsPerRestaurant) + 1,
-                Capacity = random.Next(minCapacity, maxCapacity + 1)
-            });
-        }
+                TableId = 1,
+                Capacity = 2,
+                RestaurantId = 1
+            },
+            new Table()
+            {
+                TableId = 2,
+                Capacity = 5,
+                RestaurantId = 1
+            },
+            new Table()
+            {
+                TableId = 3,
+                Capacity = 10,
+                RestaurantId = 1
+            },
+            new Table()
+            {
+                TableId = 4,
+                Capacity = 2,
+                RestaurantId = 1
+            },
+            new Table()
+            {
+                TableId = 5,
+                Capacity = 6,
+                RestaurantId = 2
+            },
+            new Table()
+            {
+                TableId = 6,
+                Capacity = 10,
+                RestaurantId = 2
+            },
+            new Table()
+            {
+                TableId = 7,
+                Capacity = 10,
+                RestaurantId = 2
+            },
+            new Table()
+            {
+                TableId = 8,
+                Capacity = 2,
+                RestaurantId = 2
+            },
+            new Table()
+            {
+                TableId = 9,
+                Capacity = 10,
+                RestaurantId = 3
+            },
+            new Table()
+            {
+                TableId = 10,
+                Capacity = 10,
+                RestaurantId = 3
+            },
+            new Table()
+            {
+                TableId = 11,
+                Capacity = 7,
+                RestaurantId = 3
+            },
+            new Table()
+            {
+                TableId = 12,
+                Capacity = 5,
+                RestaurantId = 3
+            },
+            new Table()
+            {
+                TableId = 13,
+                Capacity = 3,
+                RestaurantId = 4
+            },
+            new Table()
+            {
+                TableId = 14,
+                Capacity = 2,
+                RestaurantId = 4
+            },
+            new Table()
+            {
+                TableId = 15,
+                Capacity = 10,
+                RestaurantId = 4
+            },
+            new Table()
+            {
+                TableId = 16,
+                Capacity = 9,
+                RestaurantId = 4
+            },
+            new Table()
+            {
+                TableId = 17,
+                Capacity = 7,
+                RestaurantId = 5
+            },
+            new Table()
+            {
+                TableId = 18,
+                Capacity = 8,
+                RestaurantId = 5
+            },
+            new Table()
+            {
+                TableId = 19,
+                Capacity = 4,
+                RestaurantId = 5
+            },
+            new Table()
+            {
+                TableId = 20,
+                Capacity = 3,
+                RestaurantId = 5
+            }
+        };
 
         modelBuilder.Entity<Table>().HasData(tables);
     }
@@ -182,7 +291,7 @@ public static class DataSeeder
         modelBuilder.Entity<Employee>().HasData(employees);
     }
 
-    private static List<MenuItem> SeedMenuItemsTable(ModelBuilder modelBuilder)
+    private static void SeedMenuItemsTable(ModelBuilder modelBuilder)
     {
         var menuItems = new List<MenuItem>
         {
@@ -349,184 +458,296 @@ public static class DataSeeder
         };
 
         modelBuilder.Entity<MenuItem>().HasData(menuItems);
-        return menuItems;
     }
 
     private static void SeedReservationsTable(ModelBuilder modelBuilder)
     {
         var reservations = new List<Reservation>
         {
-            new Reservation
+            new Reservation()
             {
                 ReservationId = 1,
                 CustomerId = 1,
+                PartySize = 4,
+                ReservationDate = new DateTime(2023, 10, 10, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5249),
                 RestaurantId = 1,
-                TableId = 1,
-                ReservationDate = DateTime.Now.AddDays(1),
-                PartySize = 4
+                TableId = 1
             },
-            new Reservation
+            new Reservation()
             {
                 ReservationId = 2,
                 CustomerId = 1,
+                PartySize = 2,
+                ReservationDate = new DateTime(2023, 10, 11, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5304),
                 RestaurantId = 1,
-                TableId = 2,
-                ReservationDate = DateTime.Now.AddDays(2),
-                PartySize = 2
+                TableId = 2
             },
-            new Reservation
+            new Reservation()
             {
                 ReservationId = 3,
                 CustomerId = 3,
+                PartySize = 6,
+                ReservationDate = new DateTime(2023, 10, 12, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5308),
                 RestaurantId = 2,
-                TableId = 5,
-                ReservationDate = DateTime.Now.AddDays(3),
-                PartySize = 6
+                TableId = 5
             },
-            new Reservation
+            new Reservation()
             {
                 ReservationId = 4,
                 CustomerId = 4,
+                PartySize = 8,
+                ReservationDate = new DateTime(2023, 10, 13, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5311),
                 RestaurantId = 2,
-                TableId = 6,
-                ReservationDate = DateTime.Now.AddDays(4),
-                PartySize = 8
+                TableId = 6
             },
-            new Reservation
+            new Reservation()
             {
                 ReservationId = 5,
                 CustomerId = 5,
+                PartySize = 3,
+                ReservationDate = new DateTime(2023, 10, 14, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5315),
                 RestaurantId = 3,
-                TableId = 9,
-                ReservationDate = DateTime.Now.AddDays(5),
-                PartySize = 3
+                TableId = 9
             },
-            new Reservation
+            new Reservation()
             {
                 ReservationId = 6,
                 CustomerId = 5,
+                PartySize = 3,
+                ReservationDate = new DateTime(2023, 10, 14, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5320),
                 RestaurantId = 4,
-                TableId = 15,
-                ReservationDate = DateTime.Now.AddDays(5),
-                PartySize = 3
+                TableId = 15
             },
-            new Reservation
+            new Reservation()
             {
                 ReservationId = 7,
                 CustomerId = 5,
+                PartySize = 3,
+                ReservationDate = new DateTime(2023, 10, 14, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5323),
                 RestaurantId = 5,
-                TableId = 19,
-                ReservationDate = DateTime.Now.AddDays(5),
-                PartySize = 3
+                TableId = 19
             }
         };
         modelBuilder.Entity<Reservation>().HasData(reservations);
     }
 
-    private static void SeedOrdersAndOrderItemsTables(ModelBuilder modelBuilder, List<MenuItem> menuItems)
+    private static void SeedOrdersTable(ModelBuilder modelBuilder)
     {
-        var orderItemsMaxId = 0;
-        var orders = new List<Order>
+        var orders = new Order[]
         {
             new Order()
             {
                 OrderId = 1,
-                ReservationId = 1,
                 EmployeeId = 1,
-                OrderDate = DateTime.Now.AddDays(-1),
-                OrderItems = GenerateOrderItems(1, menuItems, ref orderItemsMaxId),
+                OrderDate = new DateTime(2023, 10, 8, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5424),
+                ReservationId = 1,
+                TotalAmount = 99.99
             },
             new Order()
             {
                 OrderId = 2,
-                ReservationId = 2,
                 EmployeeId = 1,
-                OrderDate = DateTime.Now.AddDays(-2),
-                OrderItems = GenerateOrderItems(1, menuItems, ref orderItemsMaxId),
+                OrderDate = new DateTime(2023, 10, 7, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5449),
+                ReservationId = 2,
+                TotalAmount = 81.69
             },
             new Order()
             {
                 OrderId = 3,
-                ReservationId = 3,
                 EmployeeId = 1,
-                OrderDate = DateTime.Now.AddDays(-3),
-                OrderItems = GenerateOrderItems(2, menuItems, ref orderItemsMaxId),
+                OrderDate = new DateTime(2023, 10, 6, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5460),
+                ReservationId = 3,
+                TotalAmount = 217.96
             },
             new Order()
             {
                 OrderId = 4,
-                ReservationId = 4,
                 EmployeeId = 1,
-                OrderDate = DateTime.Now.AddDays(-4),
-                OrderItems = GenerateOrderItems(2, menuItems, ref orderItemsMaxId),
+                OrderDate = new DateTime(2023, 10, 5, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5484),
+                ReservationId = 4,
+                TotalAmount = 174.75
             },
             new Order()
             {
                 OrderId = 5,
-                ReservationId = 5,
                 EmployeeId = 1,
-                OrderDate = DateTime.Now.AddDays(-5),
-                OrderItems = GenerateOrderItems(3, menuItems, ref orderItemsMaxId),
+                OrderDate = new DateTime(2023, 10, 4, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5502),
+                ReservationId = 5,
+                TotalAmount = 98.22
             },
             new Order()
             {
                 OrderId = 6,
-                ReservationId = 6,
                 EmployeeId = 1,
-                OrderDate = DateTime.Now.AddDays(-6),
-                OrderItems = GenerateOrderItems(4, menuItems, ref orderItemsMaxId),
+                OrderDate = new DateTime(2023, 10, 3, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5512),
+                ReservationId = 6,
+                TotalAmount = 49.75
             },
             new Order()
             {
                 OrderId = 7,
-                ReservationId = 7,
                 EmployeeId = 1,
-                OrderDate = DateTime.Now.AddDays(-7),
-                OrderItems = GenerateOrderItems(5, menuItems, ref orderItemsMaxId),
+                OrderDate = new DateTime(2023, 10, 2, 11, 1, 50, 514, DateTimeKind.Local).AddTicks(5521),
+                ReservationId = 7,
+                TotalAmount = 64.48
+            }
+        };
+        modelBuilder.Entity<Order>().HasData(orders);
+    }
+
+    private static void SeedOrderItemsTable(ModelBuilder modelBuilder)
+    {
+        var orderItems = new OrderItem[]
+        {
+            new OrderItem()
+            {
+                OrderItemId = 1,
+                MenuItemId = 2,
+                OrderId = 1,
+                Quantity = 4
+            },
+            new OrderItem
+            {
+                OrderItemId = 2,
+                MenuItemId = 4,
+                OrderId = 1,
+                Quantity = 1
+            },
+            new OrderItem
+            {
+                OrderItemId = 3,
+                MenuItemId = 1,
+                OrderId = 2,
+                Quantity = 2
+            },
+            new OrderItem
+            {
+                OrderItemId = 4,
+                MenuItemId = 4,
+                OrderId = 2,
+                Quantity = 2
+            },
+            new OrderItem
+            {
+                OrderItemId = 5,
+                MenuItemId = 4,
+                OrderId = 2,
+                Quantity = 2
+            },
+            new OrderItem
+            {
+                OrderItemId = 6,
+                MenuItemId = 3,
+                OrderId = 2,
+                Quantity = 1
+            },
+            new OrderItem
+            {
+                OrderItemId = 7,
+                MenuItemId = 8,
+                OrderId = 3,
+                Quantity = 4
+            },
+            new OrderItem
+            {
+                OrderItemId = 8,
+                MenuItemId = 7,
+                OrderId = 3,
+                Quantity = 4
+            },
+            new OrderItem
+            {
+                OrderItemId = 9,
+                MenuItemId = 7,
+                OrderId = 3,
+                Quantity = 4
+            },
+            new OrderItem
+            {
+                OrderItemId = 10,
+                MenuItemId = 7,
+                OrderId = 4,
+                Quantity = 1
+            },
+            new OrderItem
+            {
+                OrderItemId = 11,
+                MenuItemId = 6,
+                OrderId = 4,
+                Quantity = 3
+            },
+            new OrderItem
+            {
+                OrderItemId = 12,
+                MenuItemId = 7,
+                OrderId = 4,
+                Quantity = 2
+            },
+            new OrderItem
+            {
+                OrderItemId = 13,
+                MenuItemId = 7,
+                OrderId = 4,
+                Quantity = 4
+            },
+            new OrderItem
+            {
+                OrderItemId = 14,
+                MenuItemId = 10,
+                OrderId = 5,
+                Quantity = 3
+            },
+            new OrderItem
+            {
+                OrderItemId = 15,
+                MenuItemId = 12,
+                OrderId = 5,
+                Quantity = 1
+            },
+            new OrderItem
+            {
+                OrderItemId = 16,
+                MenuItemId = 11,
+                OrderId = 5,
+                Quantity = 4
+            },
+            new OrderItem
+            {
+                OrderItemId = 17,
+                MenuItemId = 16,
+                OrderId = 6,
+                Quantity = 1
+            },
+            new OrderItem
+            {
+                OrderItemId = 18,
+                MenuItemId = 15,
+                OrderId = 6,
+                Quantity = 4
+            },
+            new OrderItem
+            {
+                OrderItemId = 19,
+                MenuItemId = 17,
+                OrderId = 7,
+                Quantity = 2
+            },
+            new OrderItem
+            {
+                OrderItemId = 20,
+                MenuItemId = 17,
+                OrderId = 7,
+                Quantity = 1
+            },
+            new OrderItem
+            {
+                OrderItemId = 21,
+                MenuItemId = 19,
+                OrderId = 7,
+                Quantity = 2
             }
         };
 
-        var orderItems = new List<OrderItem>();
-
-        orders.ForEach(order =>
-        {
-            order.TotalAmount = order.OrderItems.Sum(item => item.Quantity * item.Item.Price);
-            order.OrderItems.ForEach(orderItem =>
-            {
-                orderItem.OrderId = order.OrderId;
-                orderItem.Item = null; // Couldn't use 'HasData()' when the object was set, I have no idea why :\
-            });
-            orderItems.AddRange(order.OrderItems);
-            order.OrderItems.Clear(); // Same here, I had to clear the list to be able to use 'HasData()'
-        });
-
-        modelBuilder.Entity<Order>().HasData(orders);
         modelBuilder.Entity<OrderItem>().HasData(orderItems);
-    }
-
-    private static List<OrderItem> GenerateOrderItems(int restaurantId, IReadOnlyList<MenuItem> menuItems,
-        ref int orderItemsMaxId)
-    {
-        var orderItems = new List<OrderItem>();
-
-        int maxNumOfItems = 5, maxQuantity = 4;
-        var random = new Random();
-
-        for (int i = 1; i <= random.Next(1, maxNumOfItems + 1); i++)
-        {
-            // the first 4 menu items belong to the first restaurant, the next 4 belong to the second one, and so on
-            var menuItemIndex = random.Next((restaurantId - 1) * 4, restaurantId * 4);
-            var orderItem = new OrderItem
-            {
-                OrderItemId = ++orderItemsMaxId,
-                MenuItemId = menuItemIndex + 1,
-                Item = menuItems[menuItemIndex],
-                Quantity = random.Next(1, maxQuantity + 1)
-            };
-
-            orderItems.Add(orderItem);
-        }
-
-        return orderItems;
     }
 }
