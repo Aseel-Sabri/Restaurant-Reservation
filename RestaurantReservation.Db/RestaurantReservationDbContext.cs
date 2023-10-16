1ac648116a70c246c4041c86faff8167e8cd3252
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-10-16 10:21:06 +0300

    Update delete constraint for orders and reservations

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -28,14 +28,27 @@ public class RestaurantReservationDbContext : DbContext
             .Property(menuItem => menuItem.Price)
             .HasColumnType("decimal(18, 2)");
 
-        modelBuilder.Entity<Order>()
-            .Property(order => order.TotalAmount)
-            .HasColumnType("decimal(18, 2)");
 
         foreach (var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
         {
             foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
         }
+
+        modelBuilder.Entity<Order>(orderBuilder =>
+            {
+                orderBuilder.Property(order => order.TotalAmount)
+                    .HasColumnType("decimal(18, 2)");
+                orderBuilder.HasMany(order => order.OrderItems)
+                    .WithOne(orderItem => orderItem.Order)
+                    .HasForeignKey(orderItem => orderItem.OrderId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                orderBuilder.HasOne(order => order.Reservation)
+                    .WithMany()
+                    .HasForeignKey(order => order.ReservationId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            }
+        );
 
         DataSeeder.SeedData(modelBuilder);
     }
